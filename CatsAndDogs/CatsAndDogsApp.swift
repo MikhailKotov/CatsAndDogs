1d1dd95b7c0a08c6e028685d75de78631d49d11a
Mykhailo Kotov <mkhlktv@gmail.com>
2024-12-27 09:29:24 +0100

    Initial Commit

---
+++ CatsAndDogs/CatsAndDogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAndDogsApp.swift
+//  CatsAndDogs
+//
+//  Created by Mykhailo Kotov on 27/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatsAndDogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
